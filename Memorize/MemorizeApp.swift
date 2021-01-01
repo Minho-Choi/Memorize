49e38d7e50f9101bbafc72423d4ac2437c93e527
Minho-Choi <2153373@naver.com>
2021-01-01 15:42:12 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Minho Choi on 2021/01/01.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
